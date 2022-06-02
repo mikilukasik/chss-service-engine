164ec835523d6262be879f8f09fd5d0c9e61480e
mikilukasik <szarhazi@gmail.com>
2022-06-02 19:37:11 +0100

    add blank service

---
+++ src/routes/routes.js
@@ -0,0 +1,6 @@
+// import { sampleRequestHandler } from "./sampleSocket/sampleRequestHandler";
+
+export const initRoutes = ({ msg }) => {
+  // const sampleSocket = msg.ws("/sampleSocket");
+  // sampleSocket.on(...sampleRequestHandler);
+};
